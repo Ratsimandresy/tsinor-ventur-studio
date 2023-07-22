1d596ba084f544903bd5883d05c09156ae9ea0ef
Ratsimandresy Rabeson <ricorabesoa@gmail.com>
2023-07-22 14:44:23 +0200

    test: green, all success cases passed

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 console.log('compiled successfully');
 
 export const getIndicesOfTheSumTwoNumbers = (nums: number[], target: number): number[] => {
-    
+	const numIndex: { [key: number]: number } = {};
+
+	for (let i = 0; i < nums.length; i++) {
+        const currentNumber = nums[i];
+        const complement = target - currentNumber;
+
+        if(numIndex.hasOwnProperty(complement)) {
+            return [numIndex[complement], i]
+        }
+
+        numIndex[currentNumber] = i;
+    }
+
 	return [0, 1];
 };
