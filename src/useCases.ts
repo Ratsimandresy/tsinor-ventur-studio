650b5114ec7398a08ffa173b815f681b21201845
Ratsimandresy Rabeson <ricorabesoa@gmail.com>
2023-07-22 15:45:09 +0200

    test: green, all success cases passed

--- src/useCases.ts
+++ src/useCases.ts
@@ -9,9 +9,27 @@ type Case = {
 	output: number[];
 };
 
-export const useCases: Case[] = [
+export const simpleCases: Case[] = [
 	{ name: 'example 1', inputs: { nums: [2, 7, 11, 15], target: 9 }, output: [0, 1] },
 	{ name: 'example 2', inputs: { nums: [3, 2, 4], target: 6 }, output: [1, 2] },
 	{ name: 'example 3', inputs: { nums: [3, 3], target: 6 }, output: [0, 1] },
 	{ name: 'handle zero and negative number', inputs: { nums: [-1, 0, 1, 3, 5, -4, 6], target: 4 }, output: [2, 3] },
+];
+
+export const moreThanTwoNumbersOccurenceCases: Case[] = [
+	{
+		name: 'two occurences of the same numbers',
+		inputs: { nums: [-1, 0, 1, 1, 3, 5, -4, 6], target: 4 },
+		output: [2, 4],
+	},
+	{
+		name: 'three occurences of the same numbers',
+		inputs: { nums: [12, 0, 6, 5, 12, 5, 5, 11], target: 11 },
+		output: [2, 3],
+	},
+	{
+		name: 'mulitple occurences of the same numbers',
+		inputs: { nums: [-1, 0, 12, 1, 13, 23, 3, 5, -4, 1, 1, 3, 3, 6, 3], target: 4 },
+		output: [3, 6],
+	},
 ];
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,21 +2,21 @@ import type * as TestFunction from '../src/index';
 import type * as TestCases from '../src/useCases';
 
 const { getIndicesOfTheSumTwoNumbers } = jest.requireActual<typeof TestFunction>('../src/index.ts');
-const { useCases } = jest.requireActual<typeof TestCases>('../src/useCases.ts');
+const { simpleCases,  moreThanTwoNumbersOccurenceCases} = jest.requireActual<typeof TestCases>('../src/useCases.ts');
 
 describe('getIndicesOfTheSumTwoNumbers', () => {
-	test.each(useCases)('should return the correct indices of the $name', ({ inputs, output }) => {
+	test.each(simpleCases)('should return the correct indices of the $name', ({ inputs, output }) => {
 		const { nums, target } = inputs;
 		const result = getIndicesOfTheSumTwoNumbers(nums, target);
 
 		expect(result).toEqual(output);
 	});
 
-	test('should not use the same number twice', () => {
-		const inputs = { nums: [-1, 0, 1, 1, 3, 5, -4, 6], target: 4 };
-		const output = [2, 4];
-		const result = getIndicesOfTheSumTwoNumbers(inputs.nums, inputs.target);
+    test.each(moreThanTwoNumbersOccurenceCases)('should return the correct indices when we have $name', ({ inputs, output }) => {
+		const { nums, target } = inputs;
+		const result = getIndicesOfTheSumTwoNumbers(nums, target);
 
 		expect(result).toEqual(output);
 	});
+
 });
